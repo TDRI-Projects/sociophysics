1cd5660804214afab1ce374fe12ae614b35e72b0
Narin <narin.tanani@gmail.com>
2023-10-10 19:14:28 +0700

    Fix

--- src/routes/schelling-model/+page.svelte
+++ src/routes/schelling-model/+page.svelte
@@ -2,10 +2,10 @@
     // @ts-ignore
     import RangeSlider from 'svelte-range-slider-pips'
 
-    let L_slider: number[] = [10]
+    let L_slider: number[] = [20]
     let T_slider: number[] = [0.3]
     let rho_slider: number[] = [0.1]
-    let delay_slider: number[] = [50]
+    let delay_slider: number[] = [200]
 
     type Cell = 'red' | 'blue' | 'empty'
 
@@ -37,23 +37,33 @@
     }
 
     function is_happy(i: number, j: number): boolean {
-        const cell = grid[i][j]
+        if (grid[j][i] === 'empty') {
+            return true
+        }
+
+        const left_i = i === 0 ? L_slider[0] - 1 : i - 1
+        const right_i = i === L_slider[0] - 1 ? 0 : i + 1
+        const top_j = j === 0 ? L_slider[0] - 1 : j - 1
+        const bottom_j = j === L_slider[0] - 1 ? 0 : j + 1
+
         const neighbors = [
-            grid[i - 1]?.[j - 1],
-            grid[i - 1]?.[j],
-            grid[i - 1]?.[j + 1],
-            grid[i]?.[j - 1],
-            grid[i]?.[j + 1],
-            grid[i + 1]?.[j - 1],
-            grid[i + 1]?.[j],
-            grid[i + 1]?.[j + 1],
+            grid[top_j][left_i],
+            grid[top_j][i],
+            grid[top_j][right_i],
+            grid[j][left_i],
+            grid[j][right_i],
+            grid[bottom_j][left_i],
+            grid[bottom_j][i],
+            grid[bottom_j][right_i],
         ].filter(x => x !== 'empty')
-        const num_same = neighbors.filter(neighbor => neighbor === cell).length
-        return num_same / neighbors.length >= T_slider[0]
+        const num_same = neighbors.filter(neighbor => neighbor === grid[j][i]).length
+
+        const happy = num_same / neighbors.length >= T_slider[0]
+        return happy
     }
 
     function get_empty_cells(): [number, number][] {
-        return grid.map((row, i) => row.map((_, j) => grid[i][j] === 'empty' ? [i, j] : false)).flat().filter(x => x !== false) as [number, number][]
+        return grid.map((row, j) => row.map((_, i) => grid[j][i] === 'empty' ? [i, j] : false)).flat().filter(x => x !== false) as [number, number][]
     }
 
     function shuffleArray(array: any) {
@@ -66,7 +76,7 @@
     async function start() {
         is_running = true
         while (is_running) {
-            const unhappy_cells = grid.map((row, i) => row.map((_, j) => is_happy(i, j) ? false : [i, j])).flat().filter(x => x !== false)
+            const unhappy_cells = grid.map((row, j) => row.map((_, i) => is_happy(i, j) ? false : [i, j])).flat().filter(x => x !== false)
             if (unhappy_cells.length == 0) {
                 break
             }
@@ -76,8 +86,8 @@
                 const [origin_i, origin_j] = unhappy_cell as [number, number]
                 const empty_cells = get_empty_cells()
                 const [target_i, target_j] = empty_cells[Math.floor(Math.random() * empty_cells.length)] as [number, number]
-                grid[target_i][target_j] = grid[origin_i][origin_j]
-                grid[origin_i][origin_j] = 'empty'
+                grid[target_j][target_i] = grid[origin_j][origin_i]
+                grid[origin_j][origin_i] = 'empty'
             }
             await sleep(delay_slider[0])
         }
@@ -92,8 +102,9 @@
     >
         {#each Array(L_slider[0]) as _, j}
             {#each Array(L_slider[0]) as _, i}
-                <div class={'cell-' + grid[L_slider[0] - j - 1][i]}>
-                    <!-- ({i}, {L_slider[0] - j - 1}) -->
+                <div class={'cell-' + grid[j][i]}>
+                    <!-- ({i}, {j})
+                    {grid[j][i]} -->
                 </div>
             {/each}
         {/each}
@@ -108,8 +119,9 @@
         <div>
             <p class="font-bold text-xl text-center">L: {L_slider[0]}</p>
             <RangeSlider
-                min={10}
+                min={20}
                 max={50}
+                step={5}
                 first=label
                 last=label
                 pips
@@ -146,9 +158,9 @@
         <div>
             <p class="font-bold text-xl text-center">Delay: {delay_slider[0]} ms</p>
             <RangeSlider
-                min={1}
-                max={50}
-                step={1}
+                min={20}
+                max={200}
+                step={10}
                 first=label
                 last=label
                 suffix=" ms"
