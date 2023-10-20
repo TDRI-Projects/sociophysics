c4d62f2759023146dd33dbd02659bef15a7c3433
Narin <narin.tanani@gmail.com>
2023-10-20 11:12:50 +0100

    Update cdf.ts

--- src/lib/utils/cdf.ts
+++ src/lib/utils/cdf.ts
@@ -1,9 +1,11 @@
 export function cdf(data: number[]): { x: number; y: number }[] {
-    const cdf_y: number[] = []
-    const cdf_x = [...Array(Math.max(...data) + 1).keys()]
-    for (const x_i of cdf_x) {
-        cdf_y.push(data.filter(x => x <= x_i).length / data.length)
+    const cdf: { x: number; y: number }[] = []
+    for (let i = 0; i < 300; i++) {
+        cdf.push({
+            x: i,
+            y: data.filter(x => x <= i).length / data.length
+        })
     }
     
-    return []
+    return cdf
 }
